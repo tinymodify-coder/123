47d410808b4c35ab3174f136eb182235fe5d3038
tinymodify <2101015497@qq.com>
2021-01-30 11:24:34 +0800

    1.28将原来的项目改为maven方便多平台运行，管理依赖（jar包）

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,3 @@
-import javafx.scene.control.Tab;
-
 import java.io.InputStream;
 import java.util.Scanner;
 
@@ -12,8 +10,8 @@ public class Test {
         while (bool) {
             System.out.println("请输入用户名：");
             Scanner sc = new Scanner(System.in);
-            String username = sc.next();//阻塞方法
 
+            String username = sc.next();//阻塞方法
             System.out.println("请输入密码：");
             String password = sc.next();
 
